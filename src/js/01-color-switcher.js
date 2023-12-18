d7de2f0997590fde8ffa5659fbdb67cdee0cf67a
Konstantyn Radchenko <radchenko1904@gmail.com>
2023-12-18 20:22:47 +0200

    markup color-swicher

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,39 @@
+import '../css/common.css';
 
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215)
+    .toString(16)
+    .padStart(6, 0)}`;
+}
+
+const refs = {
+  start: document.querySelector('[data-start]'),
+  stop: document.querySelector('[data-stop]'),
+  body: document.querySelector('body'),
+};
+
+refs.start.addEventListener('click', onStartClick);
+refs.stop.addEventListener('click', onStopClick);
+
+let timerId = null;
+refs.stop.disabled = true;
+
+function onStartClick(e) {
+  const btnStart = e.target;
+
+  btnStart.disabled = true;
+  refs.stop.disabled = false;
+
+  timerId = setInterval(() => {
+    refs.body.style.backgroundColor = getRandomHexColor();
+  }, 1000);
+}
+
+function onStopClick(e) {
+  const btnStop = e.target;
+
+  clearInterval(timerId);
+
+  btnStop.disabled = true;
+  refs.start.disabled = false;
+}
